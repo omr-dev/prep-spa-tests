64875fed63b3a401c37ebde912d530ef71a8845b
omer <46997887+omr-dev@users.noreply.github.com>
2022-05-24 13:50:45 +0200

    Spa1 (#3)
    * spa 1 html in
    
    * style of spa1 in

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import { Routes, Route, Navigate } from "react-router-dom";
 import { PageInfo } from "./sub-pages/PageInfo";
-import {PageSpaTest1} from "./sub-pages/PageSpaTest1"
-import {PageSpaTest2} from "./sub-pages/PageSpaTest2"
-import {PageSpaTest3} from "./sub-pages/PageSpaTest3"
-import {PageSpaTest4} from "./sub-pages/PageSpaTest4"
-import {PageSpaTest5} from "./sub-pages/PageSpaTest5"
-import {PageSpaTest6} from "./sub-pages/PageSpaTest6"
+import { PageSpaTest1 } from "./sub-pages/spa1/PageSpaTest1";
+import { PageSpaTest2 } from "./sub-pages/PageSpaTest2";
+import { PageSpaTest3 } from "./sub-pages/PageSpaTest3";
+import { PageSpaTest4 } from "./sub-pages/PageSpaTest4";
+import { PageSpaTest5 } from "./sub-pages/PageSpaTest5";
+import { PageSpaTest6 } from "./sub-pages/PageSpaTest6";
 
 import "./App.scss";
 
@@ -25,7 +25,6 @@ function App() {
         <Route path="spaTest4" element={<PageSpaTest4 />} />
         <Route path="spaTest5" element={<PageSpaTest5 />} />
         <Route path="spaTest6" element={<PageSpaTest6 />} />
-
       </Routes>
     </div>
   );
