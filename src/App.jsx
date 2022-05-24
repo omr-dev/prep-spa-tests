bba141c8179acb0eeaf358db925254a9ff3bb61a
omer <peynirciomerfaruk@gmail.com>
2022-05-24 13:04:28 +0200

    Navbar and Routing are active

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,34 @@
+import { Routes, Route, Navigate } from "react-router-dom";
+import { PageInfo } from "./sub-pages/PageInfo";
+import {PageSpaTest1} from "./sub-pages/PageSpaTest1"
+import {PageSpaTest2} from "./sub-pages/PageSpaTest2"
+import {PageSpaTest3} from "./sub-pages/PageSpaTest3"
+import {PageSpaTest4} from "./sub-pages/PageSpaTest4"
+import {PageSpaTest5} from "./sub-pages/PageSpaTest5"
+import {PageSpaTest6} from "./sub-pages/PageSpaTest6"
+
 import "./App.scss";
 
+import { Navbar } from "./components/Navbar";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <h1>SPA TESTS</h1>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Navigate to="info" replace />} />
+        <Route path="info" element={<PageInfo />} />
+        <Route path="spaTest1" element={<PageSpaTest1 />} />
+        <Route path="spaTest2" element={<PageSpaTest2 />} />
+        <Route path="spaTest3" element={<PageSpaTest3 />} />
+        <Route path="spaTest4" element={<PageSpaTest4 />} />
+        <Route path="spaTest5" element={<PageSpaTest5 />} />
+        <Route path="spaTest6" element={<PageSpaTest6 />} />
+
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/sub-pages/PageSpaTest1.jsx
@@ -0,0 +1,7 @@
+export const PageSpaTest1 = () => {
+  return (
+    <div className="sub-page page-spaTest1">
+      <h2>This is Spa Test 1 page</h2>
+    </div>
+  );
+};
