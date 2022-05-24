2e63e4362b301c58b66ded8d4d90e7aca567a5bc
omer <46997887+omr-dev@users.noreply.github.com>
2022-05-24 13:05:25 +0200

    Routing (#1)
    * dark theme in
    
    * Navbar and Routing are active

---
+++ src/sub-pages/PageSpaTest4.jsx
@@ -0,0 +1,7 @@
+export const PageSpaTest4 = () => {
+  return (
+    <div className="sub-page page-spaTest4">
+      <h2>This is Spa Test 4 page</h2>
+    </div>
+  );
+};
---
+++ src/sub-pages/PageSpaTest5.jsx
@@ -0,0 +1,7 @@
+export const PageSpaTest5 = () => {
+  return (
+    <div className="sub-page page-spaTest5">
+      <h2>This is Spa Test 5 page</h2>
+    </div>
+  );
+};
