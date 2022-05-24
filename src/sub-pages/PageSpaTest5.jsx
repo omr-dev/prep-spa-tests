84bb173ee2a66d263dbb1e37d2fce57ff5e5dd90
omer <46997887+omr-dev@users.noreply.github.com>
2022-05-25 00:25:41 +0200

    spa 5 in (#7)

--- src/sub-pages/PageSpaTest5.jsx
+++ src/sub-pages/PageSpaTest5.jsx
@@ -1,7 +1,17 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
 export const PageSpaTest5 = () => {
+  const [postCount, setPostCount] = useState(0);
+  useEffect(()=>{
+    (async () => {
+    const postsLengthInApi = (
+      await axios.get("https://jsonplaceholder.typicode.com/posts")
+    ).data;
+    setPostCount(postsLengthInApi.length);
+  })()},[])
   return (
     <div className="sub-page page-spaTest5">
-      <h2>This is Spa Test 5 page</h2>
+      <h2>{postCount > 0 ? `There are ${postCount} posts` : "Loading..."}</h2>
     </div>
   );
 };
