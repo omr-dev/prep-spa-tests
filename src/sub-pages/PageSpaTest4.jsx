856d4f0cb2526532f207413bd07aad37c1978c82
omer <peynirciomerfaruk@gmail.com>
2022-05-24 23:57:58 +0200

    spa 4 in

--- src/sub-pages/PageSpaTest4.jsx
+++ src/sub-pages/PageSpaTest4.jsx
@@ -1,7 +1,24 @@
+import { useState } from "react";
 export const PageSpaTest4 = () => {
+  const [topOfBox, setTopOfBox] = useState(400);
+  const moveBoxUp = () => {
+    if (topOfBox <= 0) {
+      setTopOfBox(400);
+    } else {
+      setTopOfBox(topOfBox - 50);
+    }
+  };
   return (
     <div className="sub-page page-spaTest4">
-      <h2>This is Spa Test 4 page</h2>
+      <h2>Move the Box!</h2>
+      <button
+        onClick={() => {
+          moveBoxUp();
+        }}
+      >
+        Move Up
+      </button>
+      <div className="orange-box" style={{ top: topOfBox + "px" }}></div>
     </div>
   );
 };
